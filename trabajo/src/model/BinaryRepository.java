ecde72b371f07e4b9d4642b3c1b96b8391a9ab23
PC ISABEL <isabelmatas@usal.es>
2024-11-29 17:34:50 +0100

    funciones

--- trabajo/src/model/BinaryRepository.java
+++ trabajo/src/model/BinaryRepository.java
@@ -1,7 +1,10 @@
 package model;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class BinaryRepository implements IRepository
@@ -14,6 +17,26 @@ public class BinaryRepository implements IRepository
         //
     }
 
+    public void cargarTareas() throws RepositoryException
+    {
+        File archivo = new File(ruta);
+        if(archivo.exists() && archivo.isFile())
+        {
+            try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo)))
+            {
+                tareas = (ArrayList<Task>) ois.readObject();
+            }
+            catch(IOException e)
+            {
+                throw new RepositoryException("Error al cargar las tareas");
+            }
+        }
+        else
+        {
+            tareas = new ArrayList<>();
+        }
+    }
+
     public void guardarTareas() throws RepositoryException
     {
         try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta)))
@@ -24,5 +47,28 @@ public class BinaryRepository implements IRepository
         {
             throw new RepositoryException("Error al guardar las tareas");
         }
+    }
+
+    public Task agregarTarea(Task tarea) throws RepositoryException
+    {
+        for(Task t : tareas)
+        {
+            if(t.getIdentifier() == tarea.getIdentifier())
+            {
+                throw new RepositoryException("Ya existe una tarea con ese id");
+            }
+        }
+        tareas.add(tarea);
+        guardarTareas();
+        return tarea;
+    }
+
+    public void eliminarTarea(Task tarea) throws RepositoryException
+    {
+        if(!tareas.remove(tarea))
+        {
+            throw new RepositoryException("La tarea no existe");
+        }
+        guardarTareas();
     }
 }
