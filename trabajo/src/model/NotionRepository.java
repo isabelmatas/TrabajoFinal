217205a4b973f2803be23be175113942439b2aea
PC ISABEL <isabelmatas@usal.es>
2024-12-04 17:56:11 +0100

    error

--- trabajo/src/model/NotionRepository.java
+++ trabajo/src/model/NotionRepository.java
@@ -1,6 +1,7 @@
 package model;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Map;
 
 import notion.api.v1.NotionClient;
@@ -14,6 +15,7 @@ import notion.api.v1.model.pages.PageProperty.RichText;
 import notion.api.v1.model.pages.PageProperty.RichText.Text;
 import notion.api.v1.request.databases.QueryDatabaseRequest;
 import notion.api.v1.request.pages.CreatePageRequest;
+import notion.api.v1.request.pages.UpdatePageRequest;
 
 public class NotionRepository implements IRepository
 {
@@ -29,6 +31,7 @@ public class NotionRepository implements IRepository
         this.databaseId = databaseId;
     }
 
+    @Override
     public Task addTask(Task tarea) throws RepositoryException
     {
         try
@@ -44,7 +47,7 @@ public class NotionRepository implements IRepository
             );
             PageParent parent = PageParent.database(databaseId);
             CreatePageRequest request = new CreatePageRequest(parent, properties);
-            Page response = client.createPage(request);
+            client.createPage(request);
             return tarea;
         }
         catch(Exception e)
@@ -94,6 +97,7 @@ public class NotionRepository implements IRepository
         return property;
     }
 
+    @Override
     public ArrayList<Task> getAllTask() throws RepositoryException
     {
         ArrayList<Task> tareas = new ArrayList<>();
@@ -103,8 +107,8 @@ public class NotionRepository implements IRepository
             QueryResults queryResults = client.queryDatabase(queryRequest);
             for(Page page : queryResults.getResults())
             {
-                Map<String, PageProperty> properties =page.getProperties();
-                Task tarea = mapPageToTask(page.getId(), properties);
+                Map<String, PageProperty> properties = page.getProperties();
+                Task tarea = mapPageToTask(properties);
                 if(tarea != null)
                 {
                     tareas.add(tarea);
@@ -117,5 +121,93 @@ public class NotionRepository implements IRepository
             e.printStackTrace();
         }
         return tareas;
+    }
+
+    @Override
+    public void modifyTask(Task tarea) throws RepositoryException
+    {
+        try
+        {
+            String pageId = findPageIdByIdentifier(tarea.getIdentifier());
+            if(pageId == null)
+            {
+                throw new RepositoryException("La tarea no se ha encontrado");
+            }
+            Map<String, PageProperty> updatedProperties = Map.of(
+                "title", createRichTextProperty(tarea.getTitle()),
+                "content", createRichTextProperty(tarea.getContent()),
+                "prioriry", createNumberProperty(tarea.getPriority()),
+                "date", createDateProperty(tarea.getDate().toString()),
+                "estimatedDuration", createNumberProperty(tarea.getEstimatedDuration()),
+                "completed", createCheckboxProperty(tarea.getCompleted())
+            );
+            UpdatePageRequest updateRequest = new UpdatePageRequest(pageId, updatedProperties);
+            client.updatePage(updateRequest);
+        }
+        catch(Exception e)
+        {
+            throw new RepositoryException("Error al modificar la tarea");
+        }
+    }
+
+    @Override
+    public void removeTask(Task tarea) throws RepositoryException
+    {
+        try
+        {
+            String pageId = findPageIdByIdentifier(tarea.getIdentifier());
+            if(pageId == null)
+            {
+                throw new RepositoryException("La tarea no se ha encontrado");
+            }
+            UpdatePageRequest updateRequest = new UpdatePageRequest(pageId, Collections.emptyMap(), true);
+            client.updatePage(updateRequest);
+        }
+        catch(Exception e)
+        {
+            throw new RepositoryException("Error al eliminar la tarea");
+        }
+    }
+
+    private Task mapPageToTask(Map<String, PageProperty> properties)
+    {
+        try
+        {
+            int identifier = Integer.parseInt(properties.get("identifier").getTitle().get(0).getText().getContent());
+            String title = properties.get("title").getRichText().get(0).getText().getContent();
+            String content = properties.get("content").getRichText().get(0).getText().getContent();
+            int priority = properties.get("priority").getNumber().intValue();
+            Date date = new Date(properties.get("date").getDate().getStart());
+            int estimatedDuration = properties.get("estimatedDuration").getNumber().intValue();
+            boolean completed = properties.get("completed").getCheckbox();
+            return new Task(identifier, title, date, content, priority, estimatedDuration, completed);
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
+
+    private String findPageIdByIdentifier(int identifier) throws RepositoryException
+    {
+        try
+        {
+            QueryDatabaseRequest queryRequest = new QueryDatabaseRequest(databaseId);
+            QueryResults queryResults = client.queryDatabase(queryRequest);
+
+            for (Page page : queryResults.getResults())
+            {
+                Map<String, PageProperty> properties = page.getProperties();
+                if (properties.containsKey(titleColumnName) && properties.get(titleColumnName).getTitle().get(0).getText().getContent().equals(String.valueOf(identifier)))
+                {
+                    return page.getId();
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            throw new RepositoryException("Error al buscar por identificador");
+        }
+        return null;
     }
 }
